d085cf55fa92c81469da5fa9cc082aea34d2423d
pransandip <[pransandip@gmail.com]>
2022-10-30 01:54:52 +0530

    30-OCT-2022 Sunday

---
+++ react-redux-two/src/App.js
@@ -0,0 +1,15 @@
+import ChangeDate from './ChangeDate';
+// import ReducerEx from "./ReducerEx";
+
+function App() {
+
+  return (
+    <div>
+      <ChangeDate />
+      <ChangeDate />
+      {/* <ReducerEx /> */}
+    </div>
+  );
+}
+
+export default App;
